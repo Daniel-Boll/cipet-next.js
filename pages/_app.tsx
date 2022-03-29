48117eab2a3a5bb1b3525838545f9764924b9f27
Daniel Boll <danielboll.academico@gmail.com>
2022-03-28 22:16:09 -0300

    feat(structure): Next.js + MUI v5
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import createEmotionCache from "@utils/emotionCache";
+import darkTheme from "@styles/theme/darkTheme";
+
+import "@styles/globals.css";
+
+const clientSideEmotionCache = createEmotionCache();
+
+export type _AppType = AppProps & { emotionCache: EmotionCache };
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: _AppType) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
